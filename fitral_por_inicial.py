fc7df377726ab9538e5f985097e47bb425af46da
Ernesto <ernestojrr30@gmail.com>
2024-02-29 20:45:56 -0400

    "hice el codigo"

--- fitral_por_inicial.py
+++ fitral_por_inicial.py
@@ -1,3 +1,11 @@
 # filtra nombres que comienzan con a y hacer una funcion llama filter_names_starting_with_a
+def filter_names_starting_with_a(nombre):
+   nueva_lista = filter(lambda x: not x.startswith("A"),nombre)
 
-print("hola")
+   return list(nueva_lista)
+
+nombres = list(input("Ingrese nombres seoarados por coma:").split(","))
+
+
+
+print(filter_names_starting_with_a(nombres))
